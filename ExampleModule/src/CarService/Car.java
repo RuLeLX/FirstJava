8d7cea9e92b61433ee9d66be75dc273a671d1b69
Alexander Pridava <sanyapridava@mail.ru>
2024-01-20 21:39:22 +0300

    Realiztion ExampleModule.

--- ExampleModule/src/CarService/Car.java
+++ ExampleModule/src/CarService/Car.java
@@ -1,2 +1,24 @@
-package CarService;public class Car {
+package CarService;
+
+import java.util.HashMap;
+
+public class Car {
+    private String mark;
+    private double year_born;
+    private int count_owner;
+
+    public Car(String mark, double year_born, int count_owner) {
+        this.mark = mark;
+        this.year_born = year_born;
+        this.count_owner = count_owner;
+    }
+
+    public void Fix() {}
+
+    public void Clear() {}
+
+    public void Sale() {
+        count_owner++;
+    }
+
 }
--- ExampleModule/src/CarService/CarService.java
+++ ExampleModule/src/CarService/CarService.java
@@ -1,2 +1,27 @@
-package CarService;public class CarService {
+package CarService;
+
+import java.util.Scanner;
+
+public class CarService {
+    public static void Serv(Car ClientCar) {
+        System.out.println("Для машины доступны операции: 1.Помытъ\n, 2.Починить\n, 3.Продать\n");
+        Scanner console = new Scanner(System.in);
+        int option = console.nextInt();
+
+        switch (option) {
+            case 1:
+                ClientCar.Clear();
+                System.out.println("Помыли");
+                break;
+            case 2:
+                ClientCar.Fix();
+                System.out.println("Починили");
+                break;
+            case 3:
+                ClientCar.Sale();
+                System.out.println("Продали");
+                break;
+        }
+
+    }
 }
